a4c7eb4cb6de5540d8cea0f63d468e6d318418a3
XenoCorn <>
2021-08-25 17:28:43 +0300

    v 1.1

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,53 @@ pub fn blend_u32(a: u32, b: u32, mask: u32) -> u32{ a ^ ((a ^ b) & mask) }
 pub fn blend_u64(a: u64, b: u64, mask: u64) -> u64{ a ^ ((a ^ b) & mask) }
 
 pub fn blend_u128(a: u128, b: u128, mask: u128) -> u128{ a ^ ((a ^ b) & mask) }
+
+pub fn bool_to_u8_mask(a: bool) -> u8{
+    match a{
+        true => { u8::MAX }
+        false => { 0 }
+    }
+}
+
+pub fn bool_to_u16_mask(a: bool) -> u16{
+    match a{
+        true => { u16::MAX }
+        false => { 0 }
+    }
+}
+
+pub fn bool_to_u32_mask(a: bool) -> u32{
+    match a{
+        true => { u32::MAX }
+        false => { 0 }
+    }
+}
+
+pub fn bool_to_u64_mask(a: bool) -> u64{
+    match a{
+        true => { u64::MAX }
+        false => { 0 }
+    }
+}
+
+pub fn bool_to_u128_mask(a: bool) -> u128{
+    match a{
+        true => { u128::MAX }
+        false => { 0 }
+    }
+}
+
+#[inline(always)]
+pub fn u8_mask_to_bool(a: u8) -> bool{ a > 0 }
+
+#[inline(always)]
+pub fn u16_mask_to_bool(a: u16) -> bool{ a > 0 }
+
+#[inline(always)]
+pub fn u32_mask_to_bool(a: u32) -> bool{ a > 0 }
+
+#[inline(always)]
+pub fn u64_mask_to_bool(a: u64) -> bool{ a > 0 }
+
+#[inline(always)]
+pub fn u128_mask_to_bool(a: u128) -> bool{ a > 0 }
